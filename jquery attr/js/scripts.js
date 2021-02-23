5d848719e2e8722644cf9111222dbd6ce2c813e7
Jeremy Burchfield <42611133+jeremyburchfield@users.noreply.github.com>
2021-02-22 20:08:57 -0500

    added files

---
+++ jquery attr/js/scripts.js
@@ -0,0 +1,17 @@
+$(function(){
+    $("#sw1").click(function(){
+        $(".slide").attr("src", "images/alderaan-wallpaper-001.jpg");
+    });
+
+    $("#sw2").click(function(){
+        $(".slide").attr("src", "images/galactic-timeline-005.jpg");
+    });
+
+    $("#sw3").click(function(){
+        $(".slide").attr("src", "images/hoth-wallpaper-001.jpg");
+    });
+
+    $("#sw4").click(function(){
+        $(".slide").attr("src", "images/jedi-knight-wallpaper-002.jpg");
+    });
+});
